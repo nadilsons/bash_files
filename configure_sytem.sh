eccfa5463f17e6dc40c1bb883b5b93db9439c315
Nadilson <nadilson.silva@abril.com.br>
2012-03-02 11:09:28 -0300

    ajustes no configure_system

--- configure_sytem.sh
+++ configure_sytem.sh
@@ -1,3 +1,6 @@
-ls -1 dot.* | while read line; do 
-	ln -s `pwd`/$line ~/`echo $line | sed -e 's/^dot//'`; 
+for i in './dot.*' '../dot.*'; do
+	ls -1 $i | while read line; do 
+		echo $line 
+		ln -s `pwd`/$line teste/`echo $line | sed -e 's/^\.\/dot//' | sed -e 's/^\.\.\/dot//'`; 
+	done
 done
