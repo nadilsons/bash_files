8db781a8d9144df5cadad311d83fe066f96ba930
Nadilson <nadilsons@gmail.com>
2025-06-16 19:25:11 -0300

    adding install.sh script

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
 for i in './dot.*' $CUSTOM_DIR; do
-    ls -1 $i | while read line; do 
-        echo $line 
+    ls -1 $i | while read line; do
+        echo $line
         ln -fs `pwd`/$line ~/`echo $line | sed -e 's/^\.\/dot//' | sed -e 's/^\.\.\/dot//'`;
     done
 done
+
+mkdir -p "${HOME}/.config"
+mkdir -p "${HOME}/.config/ghostty"
+
+ln -sf starship.toml "${HOME}/.config/starship.toml"
+ln -sf ghostty_config "${HOME}/.config/ghostty/config"
+
+brew install starship zsh-syntax-highlighting zsh-autosuggestions
